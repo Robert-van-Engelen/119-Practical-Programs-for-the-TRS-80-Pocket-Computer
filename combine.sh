a2ca3d72ed762177655370b8635c3d2bb2c7c27e
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2023-03-03 15:07:02 -0500

    update combine.sh script

--- combine.sh
+++ combine.sh
@@ -3,4 +3,14 @@
 # ./combine.sh prog1,bas prog2.bas ...
 
 cat =.bas $* | grep -v '^9.. "FX"' > combined.bas
-../bas2wav.sh combined.bas
+echo
+echo "Converting BAS file combined.bas to combined.wav"
+echo
+bas2img --pc=1211 combined.bas
+bin2wav --pc=1211 combined.img
+echo
+echo "Saved combined.img combined.wav"
+echo
+echo "Sending combined.wav (must turn volume up to max)"
+echo
+afplay combined.wav
